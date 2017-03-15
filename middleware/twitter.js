ad6c5b086615d190ba67326b0dafd7a939c764b3
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-03-15 19:18:38 -0400

    sloppily wrap getOauthRequestToken in a Promise

--- middleware/twitter.js
+++ middleware/twitter.js
@@ -11,8 +11,6 @@ const CLIENT_KEY = process.env.CLIENT_KEY;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const CALLBACK_URL = 'http://localhost:3000';
 
-const tokens = {};
-
 const oauth = new OAuth.OAuth(
   'https://api.twitter.com/oauth/request_token',
   'https://api.twitter.com/oauth/access_token',
@@ -23,17 +21,28 @@ const oauth = new OAuth.OAuth(
   'HMAC-SHA1'
 );
 
-oauth.getOAuthRequestToken((err, token, tokenSecret, results) => {
-  if (err) console.error(err);
-  if (!results || !results['oauth_callback_confirmed']) {
-    throw Error("There seems to have been a problem with your request");
-  }
-  tokens.appToken = token;
-  tokens.appSecret = tokenSecret;
-});
+const getRequestToken = new Promise((resolve, reject) => {
+  const tokens = {};
+  oauth.getOAuthRequestToken((err, token, tokenSecret, results) => {
+    if (err) console.error(err);
+    if (!results || !results['oauth_callback_confirmed']) {
+      reject("There seems to have been a problem with your request");
+    }
+
+    tokens.appToken = token;
+    tokens.appSecret = tokenSecret;
+    resolve(tokens);
+  });
+})
+
 
 router.use('/login', (req, res) => {
+  getRequestToken.then(tokens => {
+    console.log(tokens);
     res.send(`https://api.twitter.com/oauth/authenticate?oauth_token=${qs.escape(tokens.appToken)}`);
+  })
+
+
 });
 
 module.exports = router;
