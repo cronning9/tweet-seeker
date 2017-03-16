00f4906dd52ef4bdd49c724116ec0b2dbc140895
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-03-16 14:25:54 -0400

    get access token for user

--- middleware/twitter.js
+++ middleware/twitter.js
@@ -10,7 +10,9 @@ const OauthAsync = require('../lib/oauthAsync');
 
 const CLIENT_KEY = process.env.CLIENT_KEY;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const CALLBACK_URL = 'http://localhost:3000';
+const CALLBACK_URL = 'http://localhost:3000/twitter/auth';
+
+const tokensCache = {};
 
 const oauth = new OAuth.OAuth(
   'https://api.twitter.com/oauth/request_token',
@@ -26,8 +28,30 @@ const aOauth = new OauthAsync(oauth);
 
 router.use('/login', (req, res) => {
   aOauth.getRequestToken().then(tokens => {
-    res.send(`https://api.twitter.com/oauth/authenticate?oauth_token=${qs.escape(tokens.appToken)}`);
+    tokensCache.requestToken = tokens.requestToken;
+    tokensCache.tokenSecret = tokens.tokenSecret;
+    res.send(`https://api.twitter.com/oauth/authenticate?oauth_token=${qs.escape(tokens.requestToken)}`);
   })
+
+});
+
+router.use('/auth', (req, res) => {
+  res.redirect('/');
+  const oauthToken = req.query.oauth_token;
+  const oauthVerifier = req.query.oauth_verifier;
+
+  console.log("request token: " + oauthToken);
+  console.log("token verifier: " + oauthVerifier);
+
+  if (oauthToken !== tokensCache.requestToken) {
+    throw new Error("User token does not match app token; aborting.")
+  }
+
+  aOauth.getAccessToken(oauthToken, tokensCache.tokenSecret, oauthVerifier)
+    .then(tokens => {
+      console.log("access token: " + tokens.accessToken);
+      console.log("access token secret: " + tokens.accessTokenSecret);
+    });
 
 });
 
