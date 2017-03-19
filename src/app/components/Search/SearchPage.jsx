409cb83bb9adeb816e28ef1f6f7ebc6b8d469e25
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-03-19 01:00:25 -0400

    get geolocation

--- src/app/components/Search/SearchPage.jsx
+++ src/app/components/Search/SearchPage.jsx
@@ -4,14 +4,26 @@ import React from 'react';
 
 import userPropType from '../util/userShape.js';
 
-const SearchPage = ({user}) =>
-  <section id="search-page">
-    <section id="searchbar">
-      <h1>Find what's trending nearby</h1>
-      <input id="location" type="text" placeholder="Enter your location"/>
-      <input id="hashtag" type="text" placeholder="Look up a hashtag"/><div className="button">Submit</div>
+const SearchPage = ({user}) => {
+  let coordinates;
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(pos => coordinates = pos,
+                                             err => console.warn(err));
+  } else {
+    console.log("No location available");
+    console.log(navigator);
+  }
+
+  return (
+    <section id="search-page">
+      <section id="searchbar">
+        <h1>Find what's trending nearby</h1>
+        <input id="hashtag" type="text" placeholder="Look up a hashtag"/>
+        <div className="button">Submit</div>
+      </section>
     </section>
-  </section>
+  );
+}
 
 SearchPage.propTypes = {
   user: userPropType.isRequired
