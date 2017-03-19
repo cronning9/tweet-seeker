6f3ef263c0c4805372484153606875dd9971a0e2
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-03-18 22:24:26 -0400

    add location and status (most recent tweet) to rec'd info

--- src/app/components/Search/SearchPage.jsx
+++ src/app/components/Search/SearchPage.jsx
@@ -8,7 +8,8 @@ const SearchPage = ({user}) =>
   <section id="search-page">
     <section id="searchbar">
       <h1>Find what's trending nearby</h1>
-      <input type="text" placeholder="Look up a hashtag"/><div id="button">Submit</div>
+      <input id="location" type="text" placeholder="Enter your location"/>
+      <input id="hashtag" type="text" placeholder="Look up a hashtag"/><div className="button">Submit</div>
     </section>
   </section>
 
--- src/app/components/util/userShape.js
+++ src/app/components/util/userShape.js
@@ -6,7 +6,9 @@ const userPropType = React.PropTypes.shape({
   id: React.PropTypes.string.isRequired,
   username: React.PropTypes.string.isRequired,
   displayName: React.PropTypes.string.isRequired,
-  photos: React.PropTypes.array.isRequired
+  photos: React.PropTypes.array.isRequired,
+  location: React.PropTypes.string.isRequired,
+  status: React.PropTypes.object.isRequired
 });
 
 export default userPropType;
